2c5ebd12a88ea60b8f56caf43e46d9559bd45312
ShimaaOmar5 <121315831+ShimaaOmar5@users.noreply.github.com>
2022-12-23 22:35:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discrete_Mathematics_practical_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num, i, counter, num1, num2;
+
+            Console.Write("Please enter the first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please enter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The prime numbers between {0} and {1} are : ", num1, num2);
+
+            for (num = num1; num <= num2; num++)
+            {
+                counter = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if (counter== 0 && num != 1)
+                    Console.Write("{0} ", num);
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Second project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st Number");
+            int Num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number");
+            int Num2 = int.Parse(Console.ReadLine());
+            int i, n, sum;
+            for (n = Num1; n <= Num2; n++)
+            {
+                sum = 0;
+                for (i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        sum = sum + i;
+                    }
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
